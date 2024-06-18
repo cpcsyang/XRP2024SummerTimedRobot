950af3e7b00f751e3a53c445509618347c708abd
cpcsyang <frank.yang@cedarpark.org>
2024-06-17 23:43:10 -0700

    init XRP TimedRobot Skeleton

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,9 +6,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
+import edu.wpi.first.wpilibj.xrp.XRPMotor;
 
 
 /**
@@ -17,27 +15,15 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * the package after creating this project, you must also update the build.gradle file in the
  * project.
  */
-public class Robot extends TimedRobot
-{
-    private static final String DEFAULT_AUTO = "Default";
-    private static final String CUSTOM_AUTO = "My Auto";
-    private String autoSelected;
-    private final SendableChooser<String> chooser = new SendableChooser<>();
-    
-    private final XRPDrivetrain drivetrain = new XRPDrivetrain();
-    
+public class Robot extends TimedRobot {
+
     
     /**
      * This method is run when the robot is first started up and should be used for any
      * initialization code.
      */
     @Override
-    public void robotInit()
-    {
-        chooser.setDefaultOption("Default Auto", DEFAULT_AUTO);
-        chooser.addOption("My Auto", CUSTOM_AUTO);
-        SmartDashboard.putData("Auto choices", chooser);
-    }
+    public void robotInit() {}
     
     
     /**
@@ -62,31 +48,12 @@ public class Robot extends TimedRobot
      * chooser code above as well.
      */
     @Override
-    public void autonomousInit()
-    {
-        autoSelected = chooser.getSelected();
-        // autoSelected = SmartDashboard.getString("Auto Selector", DEFAULT_AUTO);
-        System.out.println("Auto selected: " + autoSelected);
-        
-        drivetrain.resetEncoders();
-    }
+    public void autonomousInit() {}
     
     
     /** This method is called periodically during autonomous. */
     @Override
-    public void autonomousPeriodic()
-    {
-        switch (autoSelected)
-        {
-            case CUSTOM_AUTO:
-                // Put custom auto code here
-                break;
-            case DEFAULT_AUTO:
-            default:
-                // Put default auto code here
-                break;
-        }
-    }
+    public void autonomousPeriodic() {}
     
     
     /** This method is called once when teleop is enabled. */
